69d927d6428e581b7713eb2d06495bf38921c1f9
Julen <julenjus.creative@gmail.com>
2019-03-25 12:25:55 +0100

    Menú principal a medias

---
+++ Space Invaders/Assets/Scripts/MenuHandlers/GameMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour {
+
+    public void playGame()
+    {
+        Application.LoadLevel("main_game");
+    }
+}
