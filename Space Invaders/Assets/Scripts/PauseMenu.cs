b787dc0f8eac76b8ebf431419eda39a36e12f8fd
Joselu99 <alaricoperchitaf10@gmail.com>
2019-04-04 17:34:04 +0200

    MENU PAUSA

--- Space Invaders/Assets/Scripts/PauseMenu.cs
+++ Space Invaders/Assets/Scripts/PauseMenu.cs
@@ -2,36 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
 
 	public static bool gameIsPaused;
+	public GameObject pauseMenuUI;
 
 	void Start () {
-		
+		gameIsPaused = false;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.P)) {
-
-
-
+			if(gameIsPaused)
+			{
+				resume ();
+			}
+			else{
+				pause ();
+			}
 		}
 	}
 
 	public void resume()
 	{
-		
+		Time.timeScale = 1f;
+		pauseMenuUI.SetActive (false);
+		gameIsPaused = false;
 	}
 
 	public void retry()
 	{
-
+		Time.timeScale = 1f;
+		Application.LoadLevel("main_game");
 	}
 
 	public void pause()
 	{
-
+		Time.timeScale = 0f;
+		pauseMenuUI.SetActive (true);
+		gameIsPaused = true;
 	}
 }
