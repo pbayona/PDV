a4ff05a4886afab7b556f7620857d9e73dbdfa1c
Joselu99 <alaricoperchitaf10@gmail.com>
2019-04-04 14:25:04 +0200

    menu pausa

---
+++ Space Invaders/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool gameIsPaused;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P)) {
+
+
+
+		}
+	}
+
+	public void resume()
+	{
+		
+	}
+
+	public void retry()
+	{
+
+	}
+
+	public void pause()
+	{
+
+	}
+}
